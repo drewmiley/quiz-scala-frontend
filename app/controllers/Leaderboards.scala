f9f7c3be6aa2eb7f5559ee4c417de0e0e9713d8b
Drew Miley <drewmiley92@gmail.com>
2019-05-30 23:37:19 +0100

    Attempt at hitting api on heroku

--- app/controllers/Leaderboards.scala
+++ app/controllers/Leaderboards.scala
@@ -1,19 +1,21 @@
 package controllers
 
 import javax.inject.Inject
+import play.api.Configuration
 import play.api.mvc._
 import play.api.libs.ws._
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class Leaderboards @Inject()(ws: WSClient)(implicit ec: ExecutionContext) extends Controller {
+class Leaderboards @Inject()(ws: WSClient, configuration: Configuration)(implicit ec: ExecutionContext) extends Controller {
 
   def get = Action {
     Ok(views.html.leaderboards("Leaderboards"))
   }
 
   def showSomeSiteContent = Action.async {
-    ws.url("http://localhost:8080/api").get().map { response =>
+    val api = configuration.getString("api").getOrElse("http://localhost:8080/")
+    ws.url(s"${api}api").get().map { response =>
       Ok(response.body)
     }
   }
