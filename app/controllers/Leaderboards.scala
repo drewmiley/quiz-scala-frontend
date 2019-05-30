bf3bb38763fed658640b95c2dcb8a57a51d6df4e
Drew Miley <drewmiley92@gmail.com>
2019-05-30 23:02:12 +0100

    Hits localhost api

--- app/controllers/Leaderboards.scala
+++ app/controllers/Leaderboards.scala
@@ -1,14 +1,20 @@
 package controllers
 
-import play.api.libs.ws
+import javax.inject.Inject
 import play.api.mvc._
 import play.api.libs.ws._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-object Leaderboards extends Controller {
+class Leaderboards @Inject()(ws: WSClient)(implicit ec: ExecutionContext) extends Controller {
 
   def get = Action {
     Ok(views.html.leaderboards("Leaderboards"))
+  }
+
+  def showSomeSiteContent = Action.async {
+    ws.url("http://localhost:8080/api").get().map { response =>
+      Ok(response.body)
+    }
   }
 }
