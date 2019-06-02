f18df733a58079c92d25082a71906a2182314cb3
Drew Miley <drewmiley92@gmail.com>
2019-06-02 18:59:46 +0100

    Displays leaderboard on quiz page

--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject.Inject
+import models.Leaderboard
 import play.api.mvc._
 import services.API
 
@@ -9,8 +10,9 @@ import scala.concurrent.ExecutionContext
 class Quiz @Inject()(service: API)(implicit ec: ExecutionContext) extends Controller {
 
   def get = Action.async {
-    service.getQuizByCode("040520197587").map { questions =>
-      Ok(views.html.quiz("040520197587", questions))
-    }
+    for {
+      questions <- service.getQuizByCode("040520197587")
+      leaderboard <- service.getLeaderboardByCode("040520197587")
+    } yield Ok(views.html.quiz("040520197587", questions, Leaderboard("040520197587", leaderboard)))
   }
 }
