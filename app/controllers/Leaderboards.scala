254a3605c53e27802ed0b19b6bab3885d4227491
Drew Miley <drewmiley92@gmail.com>
2019-06-02 18:10:46 +0100

    Cleaner code

--- app/controllers/Leaderboards.scala
+++ app/controllers/Leaderboards.scala
@@ -14,17 +14,14 @@ class Leaderboards @Inject()(service: API)(implicit ec: ExecutionContext) extend
       val leaderboardsWithPositions = leaderboards.map { leaderboard =>
         val positionedRows = leaderboard.rows
             .sorted(Ordering.by[LeaderboardRow, Int](_.score).reverse)
-            .zipWithIndex
-            .foldLeft(Seq[(LeaderboardRow, Int)]())((acc, d) => {
-              val row = d._1
-              val position: Int = if (acc.nonEmpty && row.score == acc.last._1.score) {
-                acc.last._1.position.get
+            .foldLeft(Seq[LeaderboardRow]())((acc, row) => {
+              val position: Int = if (acc.nonEmpty && row.score == acc.last.score) {
+                acc.last.positionAsString.toInt
               } else {
                 acc.length + 1
               }
-              acc :+ (LeaderboardRow(row.score, row.user, Some(position)), d._2)
+              acc :+ LeaderboardRow(row.score, row.user, Some(position))
             })
-            .map(_._1)
         Leaderboard(leaderboard.code, positionedRows)
       }
       Ok(views.html.leaderboards(leaderboardsWithPositions))
