0a0d9f7e3e63dae58c905a057298c0228a30ff9f
Drew Miley <drew.miley@opencastsoftware.com>
2019-06-02 10:04:54 +0100

    Renders leaderboards without positions

--- app/models/LeaderboardRow.scala
+++ app/models/LeaderboardRow.scala
@@ -3,7 +3,9 @@ package models
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-case class LeaderboardRow(score: Int = 0, user: String = "", position: Option[Int] = None)
+case class LeaderboardRow(score: Int = 0, user: String = "", position: Option[Int] = None) {
+  val positionAsString: String = position map { _.toString } getOrElse "NotFound"
+}
 
 object LeaderboardRow {
   implicit val reads: Reads[LeaderboardRow] = (
