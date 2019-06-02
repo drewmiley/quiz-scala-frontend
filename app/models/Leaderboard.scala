7cdd0423ece5657d1c6630abf5b0eb0f44da7255
Drew Miley <drewmiley92@gmail.com>
2019-06-02 19:46:00 +0100

    Moves ranking onto model

--- app/models/Leaderboard.scala
+++ app/models/Leaderboard.scala
@@ -3,7 +3,18 @@ package models
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-case class Leaderboard(code: String = "", rows: Seq[LeaderboardRow] = Seq())
+case class Leaderboard(code: String = "", rows: Seq[LeaderboardRow] = Seq()) {
+    val rowsWithPosition: Seq[LeaderboardRow] = rows
+      .sorted(Ordering.by[LeaderboardRow, Int](_.score).reverse)
+      .foldLeft(Seq[LeaderboardRow]())((acc, row) => {
+        val position: Int = if (acc.nonEmpty && row.score == acc.last.score) {
+          acc.last.positionAsString.toInt
+        } else {
+          acc.length + 1
+        }
+        acc :+ LeaderboardRow(row.score, row.user, Some(position))
+      })
+}
 
 object Leaderboard {
   implicit val reads: Reads[Leaderboard] = (
