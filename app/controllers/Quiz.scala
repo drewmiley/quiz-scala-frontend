861c48642061b5a0a12498e143b53f3706509b20
Drew Miley <drewmiley92@gmail.com>
2019-06-02 18:28:57 +0100

    Loads naive quiz

--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -1,14 +1,16 @@
 package controllers
 
 import javax.inject.Inject
-import play.api.libs.ws.WSClient
 import play.api.mvc._
+import services.API
 
 import scala.concurrent.ExecutionContext
 
-class Quiz @Inject()(ws: WSClient)(implicit ec: ExecutionContext) extends Controller {
+class Quiz @Inject()(service: API)(implicit ec: ExecutionContext) extends Controller {
 
-  def get = Action {
-    Ok(views.html.quiz("Quiz"))
+  def get = Action.async {
+    service.getQuizByCode("040520197587").map { questions =>
+      Ok(views.html.quiz("040520197587", questions))
+    }
   }
 }
