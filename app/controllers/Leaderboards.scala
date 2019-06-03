71899a87819511ae2fdea67df3f0172fc47951d7
Drew Miley <drewmiley92@gmail.com>
2019-06-03 18:35:51 +0100

    Init form

--- app/controllers/Leaderboards.scala
+++ app/controllers/Leaderboards.scala
@@ -3,6 +3,8 @@ package controllers
 import javax.inject.Inject
 import play.api.mvc._
 import services.API
+  import play.api.data._
+  import play.api.data.Forms._
 
 import scala.concurrent.ExecutionContext
 
@@ -10,6 +12,7 @@ class Leaderboards @Inject()(service: API)(implicit ec: ExecutionContext) extend
 
   def get = Action.async {
     service.getLeaderboardsByUser().map { leaderboards =>
+      val nameForm = Form("name" -> text())
       Ok(views.html.leaderboards(leaderboards))
     }
   }
