a2217f4f4154c19e1612672582f453130d7f7fee
Drew Miley <drewmiley92@gmail.com>
2019-06-03 23:12:48 +0100

    Removes use of get from quiz

--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -6,15 +6,16 @@ import play.api.cache.{NamedCache, SyncCacheApi}
 import play.api.mvc._
 import services.API
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class Quiz @Inject()(service: API, @NamedCache("session-cache") cache: SyncCacheApi)(implicit ec: ExecutionContext) extends Controller {
 
   def get = Action.async {
-    val code = cache.get[String]("code").get
-    for {
-      questions <- service.getQuizByCode(code)
-      leaderboard <- service.getLeaderboardByCode(code)
-    } yield Ok(views.html.quiz(code, questions, Leaderboard(code, leaderboard)))
+    cache.get[String]("code") map { code =>
+      for {
+        questions <- service.getQuizByCode(code)
+        leaderboard <- service.getLeaderboardByCode(code)
+      } yield Ok(views.html.quiz(code, questions, Leaderboard(code, leaderboard)))
+    } getOrElse { Future.successful(Ok(views.html.quiz())) }
   }
 }
