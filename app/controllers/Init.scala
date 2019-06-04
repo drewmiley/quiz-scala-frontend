eb14068bbb7b61178198ef1582f7dae7aee0aa5e
Drew Miley <drewmiley92@gmail.com>
2019-06-04 23:21:06 +0100

    Passes Call and Form into loadQuiz

--- app/controllers/Init.scala
+++ app/controllers/Init.scala
@@ -1,17 +1,20 @@
 package controllers
 
 import javax.inject.Inject
+import play.api.data._
+import play.api.data.Forms._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 import services.API
 
 import scala.concurrent.ExecutionContext
 
-class Init @Inject()(service: API)(implicit ec: ExecutionContext) extends Controller {
+class Init @Inject()(service: API, val messagesApi: MessagesApi)(implicit ec: ExecutionContext) extends Controller with I18nSupport {
 
-  def get = Action.async {
+  def get = Action.async { implicit request =>
     for {
       validQuizCodes <- service.getValidQuizCodes()
       validQuizOptions <- service.getValidQuizOptions
-    } yield Ok(views.html.init("", "", validQuizCodes, validQuizOptions))
+    } yield Ok(views.html.init(Form("code" -> nonEmptyText), routes.Quiz.get(), "", validQuizCodes, validQuizOptions))
   }
 }
