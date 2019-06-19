582b00bf94c2469214a97b30a99f924b15e92b92
Drew Miley <drewmiley92@gmail.com>
2019-06-19 22:48:18 +0100

    Adds dummy name input on quiz

--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -3,20 +3,25 @@ package controllers
 import javax.inject.Inject
 import models.Leaderboard
 import play.api.cache.{NamedCache, SyncCacheApi}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 import services.API
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class Quiz @Inject()(service: API, @NamedCache("session-cache") cache: SyncCacheApi)(implicit ec: ExecutionContext) extends Controller {
+class Quiz @Inject()(service: API, @NamedCache("session-cache") cache: SyncCacheApi, val messagesApi: MessagesApi)(implicit ec: ExecutionContext) extends Controller with I18nSupport {
 
   def get(code: Option[String]) = Action.async { implicit request =>
+    val nameForm = Form("name" -> nonEmptyText)
+    val name = nameForm.bindFromRequest().value.getOrElse("")
     val quizCode = code map { Some(_) } getOrElse cache.get[String]("code")
-    quizCode map { code =>
+    quizCode map { qCode =>
       for {
-        questions <- service.getQuizByCode(code)
-        leaderboard <- service.getLeaderboardByCode(code)
-      } yield Ok(views.html.quiz(code, questions, Leaderboard(code, leaderboard)))
-    } getOrElse { Future.successful(Ok(views.html.quiz())) }
+        questions <- service.getQuizByCode(qCode)
+        leaderboard <- service.getLeaderboardByCode(qCode)
+      } yield Ok(views.html.quiz(nameForm, qCode, questions, Leaderboard(qCode, leaderboard), routes.Quiz.get(code)))
+    } getOrElse { Future.successful(Ok(views.html.quiz(nameForm, getQuizUrl = routes.Quiz.get(code)))) }
   }
 }
