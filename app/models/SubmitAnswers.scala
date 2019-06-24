b1d55cf964851e1568aa1dfae692fc4d908e65fc
Drew Miley <drewmiley92@gmail.com>
2019-06-24 17:13:30 +0100

    Adds TODO

--- app/models/SubmitAnswers.scala
+++ app/models/SubmitAnswers.scala
@@ -3,6 +3,7 @@ package models
 import play.api.libs.json._
 
 case class SubmitAnswers(code: String, name: String, questions: Seq[String], answers: Seq[String]) {
+  // TODO: This should use QuestionAnswer model
   val toRequestBody: JsObject = JsObject(Seq("answers" -> JsArray(
     questions.zipWithIndex map { qI => JsObject(
       Map(
