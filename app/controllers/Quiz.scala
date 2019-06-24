fa89498a4ab43aed85da39d61c7759969006562f
Drew Miley <drewmiley92@gmail.com>
2019-06-24 11:01:49 +0100

    Displays select input for form

--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import javax.inject.Inject
-import models.Leaderboard
+import models.{Leaderboard, SubmitAnswers}
 import play.api.cache.{NamedCache, SyncCacheApi}
 import play.api.data.Form
 import play.api.data.Forms._
@@ -13,15 +13,22 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class Quiz @Inject()(service: API, @NamedCache("session-cache") cache: SyncCacheApi, val messagesApi: MessagesApi)(implicit ec: ExecutionContext) extends Controller with I18nSupport {
 
+  val submitAnswersForm: Form[SubmitAnswers] = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "questionAnswers" -> seq(nonEmptyText)
+    ) { (name, questionAnswers) => SubmitAnswers(name, questionAnswers) } {
+      submitAnswers => Some(submitAnswers.name, submitAnswers.questionAnswers)
+    }
+  )
+
   def get(code: Option[String]) = Action.async { implicit request =>
-    val nameForm = Form("name" -> nonEmptyText)
-    val name = nameForm.bindFromRequest().value.getOrElse("")
     val quizCode = code map { Some(_) } getOrElse cache.get[String]("code")
     quizCode map { qCode =>
       for {
         questions <- service.getQuizByCode(qCode)
         leaderboard <- service.getLeaderboardByCode(qCode)
-      } yield Ok(views.html.quiz(nameForm, qCode, questions, Leaderboard(qCode, leaderboard), routes.Quiz.get(code)))
-    } getOrElse { Future.successful(Ok(views.html.quiz(nameForm, getQuizUrl = routes.Quiz.get(code)))) }
+      } yield Ok(views.html.quiz(submitAnswersForm, qCode, questions, Leaderboard(qCode, leaderboard), routes.Quiz.get(code)))
+    } getOrElse { Future.successful(InternalServerError("No quiz found")) }
   }
 }
---
+++ app/models/SubmitAnswers.scala
@@ -0,0 +1,4 @@
+package models
+
+//case class SubmitAnswers(name: String, questionAnswers: Seq[QuestionAnswer])
+case class SubmitAnswers(name: String, questionAnswers: Seq[String])
